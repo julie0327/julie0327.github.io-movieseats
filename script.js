f8f5cd062aa4b1bbd5991302c88ce17e16ab4c8f
Jully Ye <jye@advin.local>
2023-03-10 10:22:13 -0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let seat = document.querySelectorAll('.seat')
-let selectedseat=document.querySelectorAll('.row .seat.selected')
 let count = document.querySelector('#count')
 let total = document.querySelector('#total')
 let price= document.querySelector('#movie')
@@ -7,20 +6,20 @@ let price= document.querySelector('#movie')
 let num=0
 for (let i = 0; i < seat.length; i++) {
     seat[i].onclick = function () {
-        if (seat[i].style.backgroundColor = '#444451')
-        {
-            seat[i].style.backgroundColor = '#6feaf6'
-            num++
+        if (seat[i].getAttribute('isSelected'))
+        {seat[i].removeAttribute('isSelected')
+            seat[i].style.backgroundColor = '#444451'
+            num--
             count.innerHTML = num
             total.innerHTML = num * price.value
         }
         else { 
-            seat[i].style.backgroundColor = '#444451'
-         num--
+            seat[i].setAttribute('isSelected',true)
+            seat[i].style.backgroundColor = '#6feaf6'
+         num++
          count.innerHTML = num
          total.innerHTML = num * price.value
-        }
-         
+        }        
     }
 }
 
